f26fbf5cd541ee95d3d09b3eb146c47e8ac61c7f
bugra yetkin <bugrayetkinn@gmail.com>
2020-06-16 17:45:43 +0300

    new

--- app/src/main/java/com/example/roomcoroutineexample/repository/Repository.kt
+++ app/src/main/java/com/example/roomcoroutineexample/repository/Repository.kt
@@ -1,10 +1,8 @@
 package com.example.roomcoroutineexample.repository
 
-import android.content.Context
 import androidx.lifecycle.LiveData
 import com.example.roomcoroutineexample.database.User
 import com.example.roomcoroutineexample.database.UserDAO
-import com.example.roomcoroutineexample.database.UserDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,11 +16,6 @@ Mail : bugrayetkinn@gmail.com
 
  */
 class Repository(val userDAO: UserDAO) {
-
-
-    fun initializeDatabase(context: Context): UserDatabase {
-        return UserDatabase.getDatabase(context)
-    }
 
     fun getAllUser(): LiveData<List<User>> = userDAO.getAllUser()
 
